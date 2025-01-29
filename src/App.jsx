67fd8a68519d797d8314d744458004fe629eebdc
Subhajit Roy <roysubhajit8697@gmail.com>
2025-01-29 20:04:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="bg-red-700">Record Player</h1>
+    </>
+  );
+}
+
+export default App;
